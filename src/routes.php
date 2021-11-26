a5d28c5d066ea4da080492d61eaa87f6d6fde891
CHARLES Nathalie <charlnathalie@gmail.com>
2021-11-26 10:26:05 +0100

    pages win loose

--- src/routes.php
+++ src/routes.php
@@ -10,6 +10,7 @@ return [
     '' => ['HomeController', 'index',],
     'play_card' => ['CardController', 'play', ['id']],
     'game' => ['HomeController', 'game'],
-    'results' => ['HomeController', 'results'],
+    'resultswin' => ['HomeController', 'resultswin'],
+    'resultslose' => ['HomeController', 'resultslose'],
     'name' => ['SessionController', 'start']
 ];
